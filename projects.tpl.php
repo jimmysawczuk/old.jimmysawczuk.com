d72b075c20e33a6d119eaad20557b4df53583ac4
jimmy <bitbucket@jimmysawczuk.com>
2011-08-07 06:39:30 -0400

    Projects page, WIP

---
+++ projects.tpl.php
@@ -0,0 +1,146 @@
+<?php
+/*
+Template Name: Projects (WIP)
+*/
+define('PAGE_TEMPLATE', 'projects');
+define('HIDE_SIDEBAR', true);
+get_header();
+?>
+<h1>Jump to</h1>
+
+A * indicates that the project is open-sourced.
+
+<ul>
+	<li><a href="#scaffolding">Scaffolding</a>*</li>
+	<li><a href="#invaders">Invaders</a>*</li>
+	<li><a href="#georgeinspace">George in Space!</a></li>
+	<li><a href="#flashcard">FlashCard</a></li>
+	<li><a href="#sleeptimer">SleepTimer</a>*</li>
+	<li><a href="#removeowly">Remove Ow.ly/Su.pr Bumper (Chrome extension)</a></li>
+	<li><a href="#bigprizeprizes">Big Prize Giveaways Prizes (Chrome extension)</a></li>
+	<li><a href="#facebookpagelookup">Facebook Page Lookup (Chrome extension)</a>*</li>
+	<li><a href="#wooter">Wooter!</a>*</li>
+	<li><a href="#realtimenotes">RealTimeNotes.com</a></li>
+</ul>
+
+<h2 id="scaffolding">Scaffolding</h2>
+A PHP-based, lightweight MVC framework for writing web applications.
+
+<h3>Source</h3>
+<ul class="nobullet">
+<li><a href="https://bitbucket.org/jimmysawczuk/scaffolding">Open-sourced at BitBucket</a></li>
+</ul>
+
+<h2 id="invaders">Invaders</h2>
+Invaders is a little game I wrote for my EECS 290 class and it turned into something kind of fun to play. Enjoy!
+
+<a href="http://www.jimmysawczuk.com/wp-content/uploads/2009/10/invaders_ss1.jpg"><img class="alignleft size-medium wp-image-400" title="invaders_ss1" src="http://www.jimmysawczuk.com/wp-content/uploads/2009/10/invaders_ss1-350x273.jpg" alt="invaders_ss1" width="252" height="196" /></a>
+<h3>Download</h3>
+<ul class="nobullet">
+	<li><a href="http://files.jimmysawczuk.com/files/invaders/setup.exe">Download Invaders 1.0.2</a> (Windows)</li>
+	<li><a href="http://files.jimmysawczuk.com/files/invaders/Invaders.ccgame">Download Invaders 1.0.1</a> (XBox 360, as a .ccgame file that you deploy to your Xbox from your computer on the same network)</li>
+</ul>
+<h3>Source</h3>
+<ul class="nobullet">
+	<li><a href="http://bitbucket.org/jimmysawczuk/invaders">Open-sourced at BitBucket</a></li>
+</ul>
+
+<h3 class="controls">Controls</h3>
+<ul>
+	<li><span style="color: #009900">PC</span>, <span style="color: #CC0000">XBox</span> controls</li>
+	<li><strong><span style="color: #009900">Left/Right arrow keys</span>, <span style="color: #CC0000">Left/Right triggers</span></strong>: rotate the turret of your gun</li>
+	<li><strong><span style="color: #009900">Spacebar</span>, <span style="color: #CC0000">A button</span></strong>: shoot (hold down Spacebar for more power)</li>
+</ul>
+
+<h2 id="georgeinspace">George In Space</h2>
+George In Space! is a quick XNA game I started as a project for my game development class and by popular demand, am uploading to the web. Enjoy!
+
+<a href="http://www.jimmysawczuk.com/wp-content/uploads/2009/10/gis_ss1.jpg"><img class="alignleft size-medium wp-image-433" title="gis_ss1" src="http://www.jimmysawczuk.com/wp-content/uploads/2009/10/gis_ss1-350x272.jpg" alt="gis_ss1" width="245" height="190" /></a>
+<h3>Download</h3>
+<ul class="nobullet">
+	<li><a href="http://files.jimmysawczuk.com/files/georgeinspace/setup_1.0.0.5.exe">Download George In Space! 1.0.0.5</a> (Windows)</li>
+	<li><a href="http://files.jimmysawczuk.com/files/georgeinspace/GeorgeInSpace.ccgame">Download George In Space! 0.9.0</a> (XBox 360, as a .ccgame file that you deploy to your Xbox from your computer on the same network)</li>
+</ul>
+
+<h3 class="controls">Controls</h3>
+<ul>
+	<li><span style="color: #009900">PC</span>, <span style="color: #CC0000">XBox</span> controls</li>
+	<li><strong><span style="color: #009900">Up/Down/Left/Right arrow keys</span>, <span style="color: #CC0000">Left thumbstick</span></strong>: move your character</li>
+	<li><strong><span style="color: #009900">Spacebar</span>, <span style="color: #CC0000">right trigger</span></strong>: spit at invisible Democrats (on PC, shoot in same direction as movement; on Xbox, control shots with right stick)</li>
+	<li><strong><span style="color: #009900">F12</span>, <span style="color: #CC0000">click the right stick</span></strong>: unhide the Democrats (to cheat)</li>
+</ul>
+
+<h2 id="flashcard">FlashCard</h2>
+This is a FlashCard program I wrote for myself to test myself on German words and phrases, but subsequently modified to some others' requests.
+<h3>Download</h3>
+<ul class="nobullet">
+	<li><a href="http://files.jimmysawczuk.com/files/flashcard/FlashCardSetup.msi">Download FlashCard 2.2.0</a></li>
+</ul>
+
+<h2 id="removeowly">Remove Ow.ly/Su.pr Bumper (Chrome extension)</h2>
+This is a Chrome extension I wrote to remove the bumper that appears on top of su.pr and ow.ly links.
+<h3>Install</h3>
+<ul class="nobullet">
+<li><a href="https://chrome.google.com/extensions/detail/nlabpdgfpdcknmimkjjhkonbfgpilpol">Install Remove Ow.ly/Su.pr Bumper from the Chrome Extension Gallery</a></li>
+</ul>
+
+<h2 id="sleeptimer">SleepTimer</h2>
+
+SleepTimer is a quick little Windows app that lets users set a timer to shut down or restart their computer, or even shut down individual programs. This allows users to, for example, go to sleep while listening to their iTunes library and their computer shuts off for them after they've fallen asleep.
+
+<a href="http://www.jimmysawczuk.com/wp-content/uploads/2011/01/sleeptimer_ss1.jpg"><img class="alignleft size-medium wp-image-433" title="gis_ss1" src="http://www.jimmysawczuk.com/wp-content/uploads/2011/01/sleeptimer_ss1.jpg" alt="gis_ss1" height="190" /></a>
+
+<h3 class="nobullet">Install</h3>
+<ul>
+	<li><a href="http://files.jimmysawczuk.com/files/sleeptimer/Setup.msi">Download SleepTimer 1.2.0</a></li>
+</ul>
+
+<h3>Source</h3>
+<ul class="nobullet">
+	<li><a href="https://bitbucket.org/jimmysawczuk/sleep-timer">Open-sourced at BitBucket</a></li>
+</ul>
+
+<h2 id="bigprizeprizes">Big Prize Giveaways Prizes (Chrome extension)</h2>
+A handy miniature <a href="http://www.bigprize.com/prizes">prizes tab</a> that shows currently-running <a href="http://www.bigprize.com/" target="_blank">Big Prize Giveaways</a> sweepstakes.
+<h3>Install</h3>
+<ul class="nobullet">
+<li><a href="https://chrome.google.com/extensions/detail/dehinmdmfgclcpijadomcogabhdceakj">Install Big Prize Giveaways Prizes from the Chrome Extension Gallery</a></li>
+</ul>
+
+<h2 id="facebookpagelookup">Facebook Page Lookup (Chrome extension)</h2>
+A quick lookup extension that provides crucial information from Facebook Pages using the Graph API. Useful for developers looking to integrate their apps with Facebook pages.
+<h3>Install</h3>
+<ul class="nobullet">
+<li><a href="https://chrome.google.com/extensions/detail/eloopmgpiphiokehjgomphnnlejikbol">Install Facebook Page Lookup from the Chrome Extension Gallery</a></li>
+</ul>
+
+<h3>Source</h3>
+<ul class="nobullet">
+<li><a href="https://bitbucket.org/jimmysawczuk/facebook-page-finder">Open-sourced at BitBucket</a></li>
+</ul>
+
+<h2 id="wooter">Wooter!</h2>
+Wooter! is a quick little app that sits in your system tray and lets you know when the item on Woot.com changes. You can customize the delay between checks if it's a Woot Off and it serves as a quick link to the site as well. Woot Off checking should happen automatically. <strike>Also, just for chuckles, it puts changes in your Event Logs.</strike> Not anymore, at least not on Windows 7. It still tries though.
+
+<img src="http://files.jimmysawczuk.com/files/images/wooter_ss_1.jpg" />
+
+<img src="http://files.jimmysawczuk.com/files/images/wooter_ss_2.jpg" />
+
+<h3>Download</h3>
+<ul class="nobullet">
+<li><a href="http://files.jimmysawczuk.com/files/wooter/setup.exe">Install Wooter 1.0.0.3</a>
+</ul>
+
+<h3>Source</h3>
+<ul class="nobullet">
+<li><a href="https://bitbucket.org/jimmysawczuk/wooter">Open-sourced at BitBucket</a></li>
+</ul>
+
+<h2 id="realtimenotes">RealTimeNotes.com</h2>
+A website that lets users start note sessions, type in notes real time, and export them afterwards. Great for running diaries for blogs. Coming at some point is the ability to embed a live session in your blog.
+
+<h3>URL</h3>
+<ul class="nobullet"><li><a href="http://www.realtimenotes.com">RealTimeNotes.com</a></li></ul>
+<?
+get_footer();
+?>
