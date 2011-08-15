d3c220fbe86cf2ea6b0ef9ef3f56d3033ff5bb8f
jimmy <bitbucket@jimmysawczuk.com>
2011-08-15 19:38:44 -0400

    Cache busting

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 		<div id="fb-root"></div>
 		<script src="//connect.facebook.net/en_US/all.js#appId=193404464015012&amp;xfbml=1" charset="utf-8"></script>
 		<script type="text/javascript" src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-		<script type="text/javascript" src="<?=get_min_url('js', true); ?>" charset="utf-8"></script>
+		<script type="text/javascript" src="<?=get_min_url('js', true); ?>&20110815" charset="utf-8"></script>
 		<script type="text/javascript">
 			var Config = { 
 				stylesheet_directory: '<? bloginfo("stylesheet_directory"); ?>',
--- js/bitbucket.js
+++ js/bitbucket.js
@@ -1,6 +1,5 @@
 var BitBucket = {
 	
-	
 	load: function(selector, cb)
 	{
 		$.get(Config.stylesheet_directory + "/ajax/bitbucket.php", {}, function(response)
