6ee33eaf0c897f31d4590742623608b50744d124
jimmy <bitbucket@jimmysawczuk.com>
2011-08-15 20:07:44 -0400

    Switching around some of the URI rewriting so my image URLs work properly.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<head>
 		<title><? bloginfo('name'); ?></title>
 		<link href='//fonts.googleapis.com/css?family=Arvo:400,700|Mako' rel='stylesheet' type='text/css' />
-		<link href="<?=get_min_url('css', true); ?>" type="text/css" rel="stylesheet" />
+		<link href="<?=get_min_url('css', true); ?>&20110815" type="text/css" rel="stylesheet" />
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 		<script src="<?=get_min_url('jquery', true); ?>" type="text/javascript" charset="utf-8"></script>
 		<meta name="viewport" content="width=1050, maximum-scale=1.0" />
