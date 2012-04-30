2366b7751fc5a22a1d2c55e666946bb94556e66e
Jimmy Sawczuk <github@jimmysawczuk.com>
2012-04-30 11:18:44 -0300

    changing %s%b to %B to show load the full commit message

--- includes/update_revision.sh
+++ includes/update_revision.sh
@@ -1,2 +1,2 @@
 git branch | sed 's/^\* \(.*\)/\1/' | egrep --regexp="^\w" > REVISION
-git log -1 --pretty=format:%h%n%h%n%H%n%ci%n%an%n%ae%n%n%s%b%n >> REVISION
+git log -1 --pretty=format:%h%n%h%n%H%n%ci%n%an%n%ae%n%n%B%n >> REVISION
