d49d1a39c53651d98e99cffaabc2b83319dcfcb6
jimmy <github@jimmysawczuk.com>
2012-12-12 21:22:53 -0500

    Adding twitter tags, excerpts

--- functions.php
+++ functions.php
@@ -96,6 +96,47 @@ function fb_og_tags()
 	echo implode("\r\n", $meta_tags);
 }
 
+function twitter_card_tags()
+{
+	$meta_tags = array();
+
+	if (is_single())
+	{
+		global $post;
+
+		$matches = array();
+		$matched = preg_match_all('#<img(?:.*)src\=\"(.+?)\"(?:.*)\/\>#i', $post->post_content, $matches);
+
+		if ($matched)
+		{
+			$meta_tags []= '<meta property="twitter:image" content="'.$matches[1][0].'" />';
+		}
+		else
+		{
+			$meta_tags []= '<meta property="twitter:image" content="'.get_bloginfo('stylesheet_directory').'/images/about_jimmy.jpg" />';
+		}
+
+		$description = extract_excerpt();
+
+		$meta_tags []= '<meta property="twitter:title" content="'.str_replace("\"", "&quot;", get_the_title()).'"/>';
+		$meta_tags []= '<meta property="twitter:type" content="summary" />';
+		$meta_tags []= '<meta property="twitter:url" content="'.get_permalink().'"/>';
+		$meta_tags []= '<meta property="twitter:description" content="'.get_excerpt().'" />';
+	}
+	else
+	{
+		$description = "A blog about baseball, technology, politics and life by Jimmy Sawczuk, a software engineer from Cleveland living in Columbia, SC.";
+
+		$meta_tags []= '<meta property="twitter:title" content="Cleveland, Curveballs and Common Sense"/>';
+		$meta_tags []= '<meta property="twitter:type" content="website"/>';
+		$meta_tags []= '<meta property="twitter:url" content="'.get_bloginfo('url').'"/>';
+		$meta_tags []= '<meta property="twitter:image" content="'.get_bloginfo('stylesheet_directory').'/images/about_jimmy.jpg" />';
+		$meta_tags []= '<meta property="twitter:description" content="'.$description.'" />';
+	}
+
+	echo implode("\r\n", $meta_tags);
+}
+
 function blog_domain($root = true)
 {
 	$url = get_bloginfo('url');
@@ -116,4 +157,20 @@ function blog_domain($root = true)
 
 		return $url;	
 	}
+}
+
+public function extract_excerpt()
+{
+	$excerpt = get_the_excerpt();
+
+	if ($excerpt == "")
+	{
+		$excerpt = get_the_post();
+
+		$excerpt = strip_tags($excerpt);
+
+		$excerpt = substr($excerpt, 0, stripos($excerpt, " ", 200));
+	}
+
+	return $excerpt;
 }
