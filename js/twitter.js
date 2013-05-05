a4bdc9338007e74259c25b49d9c878d878f20cbe
Jimmy Sawczuk <me@jimmysawczuk.com>
2013-05-04 21:07:20 -0400

    Moving back to ajax instead of JSONP

--- js/twitter.js
+++ js/twitter.js
@@ -19,15 +19,10 @@ var Twitter = (function($)
 			return;
 		}
 
-		$s = $('<script />')
-			.attr({
-				'src': Config.stylesheet_directory + "/ajax/twitter-feed.php?username=" + opts.screen_name + '&count=' + opts.count + '&callback=Twitter.render',
-				'type': 'text/javascript'
-			}).prop({
-				'async': true
-			});
-
-		$('body').append($s);
+		$.get(Config.stylesheet_directory + "/ajax/twitter-feed.php", { username: opts.screen_name, count: opts.count }, function(response)
+		{
+			render(response);
+		}, 'json');
 	}
 
 	function render(response)
